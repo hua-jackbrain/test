04daf05bccaaad4603dd75acc5b86869fa9a9172
hua-jackbrain <3104618774@qq.com>
2018-06-10 14:17:29 +0800

    delete all

--- bundle.js
+++
@@ -1,7 +0,0 @@
-var div = document.getElementById('div')
-div.style.backgroundColor = "#666";
-let name = 'huahau'
-
-function getname(name) {
-	return name
-}
--- dist.js
+++
@@ -1,2 +0,0 @@
-let name = "huazai"
-console.log(name)
